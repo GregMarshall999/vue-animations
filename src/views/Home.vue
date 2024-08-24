3719e14c6a38751c924a20a47f2daa96ae47be96
GregMarshall999 <gregory.marshall999@gmail.com>
2024-08-24 20:34:55 +0200

    css keyframes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,13 +43,12 @@ export default {
     transform: translateY(0);
 }
 .toast-enter-active {
-    transition: all 0.3s ease;
+    animation: wobble 0.5s ease;
 }
 
 .toast-leave-from {
     opacity: 1;
     transform: translateY(0);
-    
 }
 .toast-leave-to {
     opacity: 0;
@@ -60,4 +59,19 @@ export default {
 }
 /* comment factoriser? */
 
+@keyframes wobble {
+    0% { 
+        opacity: 0; 
+        transform: translateY(-60px);
+    }
+    50% { 
+        opacity: 1; 
+        transform: translateY(0);
+    }
+    60% { transform: translateX(8px); }
+    70% { transform: translateX(-8px); }
+    80% { transform: translateX(4px); }
+    90% { transform: translateX(-4px); }
+    100% { transform: translateX(0); }
+}
 </style>
