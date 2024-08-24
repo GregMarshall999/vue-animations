edec63abbc8ce5e7ab5ee82e7fe48886c1ba2a84
GregMarshall999 <gregory.marshall999@gmail.com>
2024-08-24 20:27:03 +0200

    toast animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="home">
-        <Toast v-if="showToast"/>
+        <transition name="toast">
+            <Toast v-if="showToast"/>
+        </transition>
         <Todos @badValue="triggerToast"/>
     </div>
 </template>
@@ -31,5 +33,31 @@ export default {
     display: flex;
     justify-content: center;
 }
+
+.toast-enter-from {
+    opacity: 0;
+    transform: translateY(-60px);
+}
+.toast-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+}
+.toast-enter-active {
+    transition: all 0.3s ease;
+}
+
+.toast-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+    
+}
+.toast-leave-to {
+    opacity: 0;
+    transform: translateY(-60px);
+}
+.toast-leave-active {
+    transition: all 0.3s ease;
+}
+/* comment factoriser? */
 
 </style>
