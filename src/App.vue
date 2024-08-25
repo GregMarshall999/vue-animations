53327bb8b52cb24726869659c0200d6d0484e6fd
GregMarshall999 <gregory.marshall999@gmail.com>
2024-08-25 11:59:25 +0200

    2 direction slide

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,38 @@
     <router-link to="/contact">Contact</router-link>
   </nav>
   <router-view v-slot="{ Component }">
-    <transition name="route" mode="out-in">
+    <transition :name="transitionName" mode="out-in">
       <component :is="Component"></component>
     </transition>
   </router-view>
 </template>
+
+<script setup>
+import { computed, ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const previousRoute = ref(route.fullPath);
+
+const transitionName = computed(() => {
+  //const routesOrder = ['/', '/about', '/contact'];
+  //console.log(router.getRoutes().map(r => r.path));
+  const routesOrder = router.getRoutes().map(r => r.path);
+
+  const currentIndex = routesOrder.indexOf(route.path);
+  const previousIndex = routesOrder.indexOf(previousRoute.value);
+
+  previousRoute.value = router.currentRoute.value.fullPath;
+
+  if(currentIndex < previousIndex) {
+    return 'route-right';
+  } 
+  else {
+    return 'route-left';
+  }
+});
+</script>
 
 <style>
 body {
@@ -31,18 +58,28 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 
-.route-enter-from {
+.route-left-enter-from {
   opacity: 0;
   transform: translateX(100px);
 }
-.route-leave-to {
+.route-left-leave-to {
   opacity: 0;
   transform: translateX(-100px);
 }
-.route-enter-active {
+
+.route-right-enter-from {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-right-leave-to {
+  opacity: 0;
+  transform: translateX(100px);
+}
+
+.route-left-enter-active, .route-right-enter-active {
   transition: all 0.3s ease-out;
 }
-.route-leave-active {
+.route-left-leave-active, .route-right-leave-active {
   transition: all 0.3s ease-in;
 }
 </style>
