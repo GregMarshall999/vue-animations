848eb3b6e482ec8eaa2bfc49347bd967948f12a6
GregMarshall999 <gregory.marshall999@gmail.com>
2024-08-25 11:13:41 +0200

    gsap

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,43 @@
 <template>
     <div class="about">
-        <h1>A Propos</h1>
+        <transition 
+            appear
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @after-enter="afterEnter"
+        >
+            <h1>A Propos</h1>
+        </transition>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     </div>
 </template>
+
+<script setup>
+//lien gsap: https://gsap.com/
+import gsap from 'gsap';
+
+const beforeEnter = e => {
+    console.log('before enter - set initial state');
+    e.style.transform = 'translateY(-60px)';
+    e.style.opacity = 0;
+};
+const enter = (e, done) => {
+    console.log('enter start - make transition');
+    gsap.to(e, {
+        duration: 1,
+        y: 0,
+        opacity: 1, 
+        ease: 'bounce.out', 
+        onComplete: done //add done after showing afterEnter
+    });
+};
+const afterEnter = () => {
+    console.log('after enter');
+};
+
+</script>
 
 <style>
 .about {
